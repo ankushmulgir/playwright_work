c1708bedd8f631acfd9ed6f13efa30942fa15614
ankushmulgir <41064671+ankushmulgir@users.noreply.github.com>
2024-02-25 16:19:58 +0530

    config file added

---
+++ playwright_work/src/main/java/config/config_env.java
@@ -0,0 +1,8 @@
+package config;
+
+public class config_env {
+	
+	public static boolean headless_flag = false;
+	
+
+}
--- playwright_work/src/main/java/setup/setup_browser.java
+++ playwright_work/src/main/java/setup/setup_browser.java
@@ -3,6 +3,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
+import config.config_env;
 
 /*
  * Ankush Mulgir - Date - 25th Feb, 2024
@@ -15,21 +16,21 @@ public class setup_browser {
 	public static Browser launch_browser(String browser_name){
 		Playwright playwright = Playwright.create();
 		System.out.println("\nStarted Setup Browser Method....");
-		if (browser_name.toLowerCase().equals("Chrome")) {
+		if (browser_name.toLowerCase().equals("chrome")) {
 			System.out.println("\nLaunching Chrome Browser");
-			Browser bw = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			Browser bw = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(config_env.headless_flag));
 			return bw;
 		}
 		
 		else if (browser_name.toLowerCase().equals("webkit")) {
 			System.out.println("\nLaunching Webkit Browser");
-			Browser bw = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			Browser bw = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(config_env.headless_flag));
 			return bw;
 		}
 		
 		else if (browser_name.toLowerCase().equals("firefox")) {
 			System.out.println("\nLaunching Firefox Browser");
-			Browser bw = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			Browser bw = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(config_env.headless_flag));
 			return bw;
 		}
 		
@@ -37,7 +38,7 @@ public class setup_browser {
 			System.out.println("\nLaunching Edge Browser");
 			LaunchOptions lp = new LaunchOptions();
 			lp.setChannel("msedge");
-			lp.setHeadless(false);
+			lp.setHeadless(config_env.headless_flag);
 			Browser bw = playwright.chromium().launch(lp);
 			return bw;
 		}
